cf326cd4986bae88f58fa8dd753ed50da848fa6c
nonlox200 <kolychevvladimir200910@gmail.com>
2023-03-05 19:55:40 +0300

    grt

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleSwitch = document.querySelector('theme-switch input[type="chekbox"]');
+function switchTheme(e){
+  if (e.target.checked){
+document. documentElement.setAttribute('data-theme','dark');
+}
+else{
+    document.documentElement.setAttribute('data-theme','light');}
+toggleSwitch.addEventListener('change', switchTheme, false) 
+}
+
+
+
+
+
+
+
+
+
+
